2fd0c16826c851358e4ca7c0e26e7d4a91b69333
Gil Veloza <gilveloza@gmail.com>
2026-01-11 23:53:17 +0000

    Configure for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  distDir: 'docs',
-  basePath: '',
-  assetPrefix: '',
-  images: {
-    unoptimized: true,
-  },
 }
 
 module.exports = nextConfig
